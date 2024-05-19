6416362cb3418c2019d3dec14c470872b91a6ea3
Guilherme <guilhermelopes035@gmail.com>
2024-05-19 13:18:28 -0300

    generic save

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { DynamoDBClientConfig } from '@aws-sdk/client-dynamodb'
 import { DynamoDB } from './services/DynamoDB/DynamoDB'
-import { timeStamp } from 'console'
-
-// const config: DynamoDBClientConfig = {
-//   region: 'us-east-1',
-//   credentials: {
-//     accessKeyId: 'AKIAQA2S5LV27U7C3766',
-//     secretAccessKey: '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'
-//   }
-// }
 
 const main = async () => {
   const repository = new DynamoDB('caronas')
